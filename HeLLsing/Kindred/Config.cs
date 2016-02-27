a3a97e3e77df38c628ef49b3a03ae4d881cc6101
tradana <96blackking96@gmail.com>
2016-02-27 04:18:26 +0200

    Elobuddy-Türkçe Çeviriler
    Elobuddy Addonları Türkçe Sizin İçin Çeviriyorum
    TRAdana

---
+++ HeLLsing/Kindred/Config.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Linq;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+
+// ReSharper disable InconsistentNaming
+// ReSharper disable MemberHidesStaticFromOuterClass
+
+namespace Kindred
+{
+    public static class Config
+    {
+        public const string MenuName = "Kindred";
+        private static readonly Menu Menu;
+
+        static Config()
+        {
+            // Initialize menu
+            Menu = MainMenu.AddMenu(MenuName, MenuName + "_hellsing-çeviri tradana");
+
+            // Initialize sub menus
+            Modes.Initialize();
+            Misc.Initialize();
+        }
+
+        public static void Initialize()
+        {
+        }
+
+        public static class Modes
+        {
+            public const string MenuName = "Modlar";
+            private static readonly Menu Menu;
+
+            static Modes()
+            {
+                // Initialize menu
+                Menu = Config.Menu.AddSubMenu(MenuName);
+
+                // Initialize sub groups
+                Combo.Initialize();
+                Menu.AddSeparator();
+                Harass.Initialize();
+                Menu.AddSeparator();
+                LaneClear.Initialize();
+                Menu.AddSeparator();
+                JungleClear.Initialize();
+                Menu.AddSeparator();
+                Flee.Initialize();
+            }
+
+            public static void Initialize()
+            {
+            }
+
+            public static class Combo
+            {
+                public const string GroupName = "Kombo";
+
+                private static readonly CheckBox _useQ;
+                private static readonly CheckBox _useW;
+                private static readonly CheckBox _useE;
+                private static readonly CheckBox _useItems;
+
+                private static readonly CheckBox _advancedE;
+
+                private static readonly CheckBox _gapcloseQ;
+                private static readonly Slider _triggerDistanceW;
+
+                public static bool UseQ
+                {
+                    get { return _useQ.CurrentValue; }
+                }
+                public static bool UseW
+                {
+                    get { return _useW.CurrentValue; }
+                }
+                public static bool UseE
+                {
+                    get { return _useE.CurrentValue; }
+                }
+                public static bool UseItems
+                {
+                    get { return _useItems.CurrentValue; }
+                }
+
+                public static bool UseAdvancedE
+                {
+                    get { return _advancedE.CurrentValue; }
+                }
+
+                public static bool GaploseQBeforeW
+                {
+                    get { return _useQ.CurrentValue && _gapcloseQ.CurrentValue; }
+                }
+                public static int TriggerDistanceW
+                {
+                    get { return _triggerDistanceW.CurrentValue; }
+                }
+
+                static Combo()
+                {
+                    // Initialize group
+                    Menu.AddGroupLabel(GroupName);
+
+                    _useQ = Menu.Add("comboUseQ", new CheckBox("Kullan Q"));
+                    _useW = Menu.Add("comboUseW", new CheckBox("Kullan W"));
+                    _useE = Menu.Add("comboUseE", new CheckBox("Kullan E"));
+                    _useItems = Menu.Add("comboUseItems", new CheckBox("İtemleri Kullan"));
+
+                    Menu.AddLabel("Gelişmiş Seçenekler:");
+
+                    _advancedE = Menu.Add("comboAdvancedE", new CheckBox("3 kişiyi etkileyeceğinde kullan"));
+                    _gapcloseQ = Menu.Add("comboGapcloseQ", new CheckBox("Q kullanmadan önce W kullan"));
+                    _triggerDistanceW = Menu.Add("comboDistanceW", new Slider("W hedefe vurabileceğinde başlat",
+                        (int) SpellManager.W.Range / 2, // Default
+                        (int) SpellManager.W.Range / 5, // Min
+                        (int) SpellManager.W.Range)); // Max
+                }
+
+                public static void Initialize()
+                {
+                }
+            }
+
+            public static class Harass
+            {
+                public const string GroupName = "Dürtme";
+
+                private static readonly CheckBox _useW;
+
+                public static bool UseW
+                {
+                    get { return _useW.CurrentValue; }
+                }
+
+                static Harass()
+                {
+                    // Initialize group
+                    Menu.AddGroupLabel(GroupName);
+
+                    _useW = Menu.Add("harassUseW", new CheckBox("Kullan W"));
+                }
+
+                public static void Initialize()
+                {
+                }
+            }
+
+            public static class LaneClear
+            {
+                static LaneClear()
+                {
+                }
+
+                public static void Initialize()
+                {
+                }
+            }
+
+            public static class JungleClear
+            {
+                static JungleClear()
+                {
+                }
+
+                public static void Initialize()
+                {
+                }
+            }
+
+            public static class Flee
+            {
+                static Flee()
+                {
+                }
+
+                public static void Initialize()
+                {
+                }
+            }
+        }
+
+        public static class Misc
+        {
+            public const string MenuName = "Ek Özellikler";
+            private static readonly Menu Menu;
+
+            private static readonly Slider _directionQ;
+
+            public static DirectionQ QDirection
+            {
+                get { return (DirectionQ) _directionQ.CurrentValue; }
+            }
+
+            static Misc()
+            {
+                // Initialize menu
+                Menu = Config.Menu.AddSubMenu(MenuName);
+
+                Menu.AddGroupLabel("Q Ayarları");
+                (_directionQ = new Slider("Jump direction: " + ((DirectionQ) 0), 0, 0, Enum.GetValues(typeof (DirectionQ)).Length - 1)).OnValueChange +=
+                    delegate { _directionQ.DisplayName = "Jump direction: " + ((DirectionQ) _directionQ.CurrentValue); };
+                Menu.Add("directionQ", _directionQ);
+                Menu.AddLabel("Available directions:");
+                foreach (var direction in Enum.GetValues(typeof (DirectionQ)).Cast<DirectionQ>())
+                {
+                    Menu.AddLabel(string.Format("    {0}: {1}", (int) direction, direction));
+                }
+            }
+
+            public static void Initialize()
+            {
+            }
+
+            public enum DirectionQ
+            {
+                ToTarget,
+                ToMouse
+            }
+        }
+    }
+}
---
+++ HeLLsing/MasterMind/MasterMind.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Rendering;
+using MasterMind.Components;
+
+namespace MasterMind
+{
+    public static class MasterMind
+    {
+        public static readonly TextureLoader TextureLoader = new TextureLoader();
+
+        public static bool IsSpectatorMode { get; private set; }
+
+        public static Menu Menu { get; private set; }
+
+        private static readonly IComponent[] Components =
+        {
+            new CooldownTracker(),
+            new WardTracker()
+        };
+
+        public static void Main(string[] args)
+        {
+            // Load the addon in a real match and when spectating games
+            Loading.OnLoadingComplete += OnLoadingComplete;
+            Loading.OnLoadingCompleteSpectatorMode += OnLoadingComplete;
+        }
+
+        private static void OnLoadingComplete(EventArgs args)
+        {
+            // Initialize menu
+            Menu = MainMenu.AddMenu("MasterMind", "MasterMind", "MasterMind - Improve Yourself!");
+
+            Menu.AddGroupLabel("MasterMind'e hoşgeldin, iyi vakit geçirmen dileğiyle");
+            Menu.AddLabel("Bu addon sizin oyun deneyiminizi geliştirecek bazı özellikler sunuyor");
+            Menu.AddLabel("FPS drop yapmadan eğlenebilirsiniz");
+            Menu.AddSeparator();
+            Menu.AddLabel("Alt Menulere göz atin, kendinize göre düzenleyin, gerek yok bildiğin ayarlar işte onu göster bunu göster");
+
+            // Initialize properties
+            IsSpectatorMode = Bootstrap.IsSpectatorMode;
+
+            // Initialize components
+            foreach (var component in Components.Where(component => component.ShouldLoad(IsSpectatorMode)))
+            {
+                component.InitializeComponent();
+            }
+        }
+    }
+}
