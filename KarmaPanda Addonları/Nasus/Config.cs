5349fb09593dfd7c6ac00ae5780104b5b91b6145
tradana <96blackking96@gmail.com>
2016-03-01 23:24:57 +0200

    KarmaPanda Nasus Güncellendi
    KarmaPanda Nasus Güncellendi

--- KarmaPanda Addonları/Nasus/Config.cs
+++ KarmaPanda Addonları/Nasus/Config.cs
@@ -3,12 +3,12 @@ using EloBuddy.SDK.Menu.Values;
 
 namespace Nasus
 {
-    class Config
+    internal class Config
     {
         /// <summary>
         /// Initializes the Menu
         /// </summary>
-        public static Menu ConfigMenu, FarmMenu, ComboMenu, KillStealMenu, DrawingMenu;
+        public static Menu ConfigMenu, FarmMenu, ComboMenu, DrawingMenu;
 
         /// <summary>
         /// Creates the Menu
@@ -18,7 +18,8 @@ namespace Nasus
             // Main Menu
             ConfigMenu = MainMenu.AddMenu("KA Nasus", "ConfigMenu");
             ConfigMenu.AddGroupLabel("Bu Addon KarmaPandaya aittir.");
-            ConfigMenu.AddGroupLabel("Any unauthorized redistribution without credits will result in severe consequences.");
+            ConfigMenu.AddGroupLabel(
+                "Any unauthorized redistribution without credits will result in severe consequences.");
             ConfigMenu.AddGroupLabel("Kullandığınız için teşekkürler iyi eğlenceler dilerim!");
             ConfigMenu.AddLabel("Çeviri TRAdana");
 
@@ -35,7 +36,7 @@ namespace Nasus
             FarmMenu.AddLabel("LaneTemizleme Ayarları");
             FarmMenu.Add("useELC", new CheckBox("E Kullan"));
             FarmMenu.Add("useELCS", new Slider("E için minyon say", 2, 1, 6));
-            FarmMenu.Add("manaELC", new Slider("Lanetemizleme için gereken mana %", 30));
+            FarmMenu.Add("manaELC", new Slider("Lanetemizleme E için gereken mana %", 30));
 
             // Combo Menu
             ComboMenu = ConfigMenu.AddSubMenu("Kombo", "Combo");
@@ -50,18 +51,22 @@ namespace Nasus
             ComboMenu.AddGroupLabel("R Ayarları");
             ComboMenu.Add("hpR", new Slider("R için canım şundan az", 25));
             ComboMenu.Add("intR", new Slider("R için Şu kadar düşman", 1, 0, 5));
-            ComboMenu.Add("rangeR", new Slider("R için Şu menzilde yukarıdaki kadar düşman varsa , 1200, 0, 2000));
-
-            // Kill Steal Menu
-            KillStealMenu = ConfigMenu.AddSubMenu("Kill Çalma", "KillSteal");
-            KillStealMenu.AddGroupLabel("Büyü Ayarları");
-            KillStealMenu.Add("useE", new CheckBox("E Kullan"));
+            ComboMenu.Add("rangeR", new Slider("R için Şu menzilde yukarıdaki kadar düşman varsa", 1200, 0, 2000));
 
             // Drawing Menu
-            DrawingMenu = ConfigMenu.AddSubMenu("Göstergeler", "Drawing");
-            DrawingMenu.AddGroupLabel("Büyü Ayarları");
+            DrawingMenu = ConfigMenu.AddSubMenu("Gösterge", "Drawing");
+            DrawingMenu.AddGroupLabel("Büyü Drawing Ayarları");
             DrawingMenu.Add("drawW", new CheckBox("Göster W Menzili", false));
             DrawingMenu.Add("drawE", new CheckBox("Göster E Menzili", false));
+            DrawingMenu.AddLabel("HasarTespitçisi");
+            DrawingMenu.Add("draw.Damage", new CheckBox("Göster Hasar"));
+            DrawingMenu.Add("draw.Q", new CheckBox("Q Hasarı Hesapla"));
+            DrawingMenu.Add("draw.E", new CheckBox("E Hesapla Hesapla"));
+            DrawingMenu.AddLabel("HasarTespitçisi için renk");
+            DrawingMenu.Add("draw_Alpha", new Slider("Alpha: ", 255, 0, 255));
+            DrawingMenu.Add("draw_Red", new Slider("Kırmızı: ", 255, 0, 255));
+            DrawingMenu.Add("draw_Green", new Slider("Yeşil: ", 0, 0, 255));
+            DrawingMenu.Add("draw_Blue", new Slider("Mavi: ", 0, 0, 255));
         }
     }
 }
