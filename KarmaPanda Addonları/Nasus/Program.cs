a93e977c0b95ced93907c2c905c76dc8f2205d84
tradana <96blackking96@gmail.com>
2016-03-01 11:23:29 +0200

    KarmaPanda Nasus fixlediğinden repoya eklendi
    KarmaPanda Nasus fixlediğinden repoya eklendi

---
+++ KarmaPanda Addonları/Nasus/Program.cs
@@ -0,0 +1,176 @@
+﻿namespace Nasus
+{
+    using System;
+
+    using EloBuddy;
+    using EloBuddy.SDK;
+    using EloBuddy.SDK.Enumerations;
+    using EloBuddy.SDK.Events;
+    using EloBuddy.SDK.Menu.Values;
+    using EloBuddy.SDK.Rendering;
+
+    /// <summary>
+    /// Main Class
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Spell Q
+        /// </summary>
+        public static Spell.Active Q;
+
+        /// <summary>
+        /// Spell W
+        /// </summary>
+        public static Spell.Targeted W;
+
+        /// <summary>
+        /// Spell E
+        /// </summary>
+        public static Spell.Skillshot E;
+
+        /// <summary>
+        /// Spell R
+        /// </summary>
+        public static Spell.Active R;
+
+        /// <summary>
+        /// Called when the Program Starts
+        /// </summary>
+        private static void Main()
+        {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+        }
+
+        /// <summary>
+        /// Called when the Game finished Loading
+        /// </summary>
+        /// <param name="args">The Args</param>
+        private static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            if (Player.Instance.Hero != Champion.Nasus)
+            {
+                return;
+            }
+
+            Q = new Spell.Active(SpellSlot.Q, 150);
+            W = new Spell.Targeted(SpellSlot.W, 600);
+            E = new Spell.Skillshot(SpellSlot.E, 650, SkillShotType.Circular, 500, 20, 380)
+            {
+                AllowedCollisionCount = int.MaxValue
+            };
+            R = new Spell.Active(SpellSlot.R);
+
+            // Methods
+            Config.Initialize();
+            Chat.Print("KA Nasus: Loaded", System.Drawing.Color.Green);
+
+            // Events
+            Game.OnTick += Game_OnTick;
+            Game.OnTick += StateHandler.KillSteal;
+            Drawing.OnDraw += Drawing_OnDraw;
+            Orbwalker.OnPostAttack += Orbwalker_OnPostAttack;
+            Orbwalker.OnPreAttack += Orbwalker_OnPreAttack;
+        }
+
+        private static void Orbwalker_OnPostAttack(AttackableUnit target, EventArgs args)
+        {
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo) ||
+                Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Harass))
+            {
+                var t = target as AIHeroClient;
+                var useQ = Config.ComboMenu["useQ"].Cast<CheckBox>().CurrentValue;
+                var useQh = Config.FarmMenu["useQH"].Cast<CheckBox>().CurrentValue;
+
+                if (t != null && (useQ || useQh))
+                {
+                    if (t.IsValidTarget() && Q.IsReady())
+                    {
+                        Q.Cast();
+                        Orbwalker.ForcedTarget = target;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Called whenever a target is about to be attacked by Orbwalker.
+        /// </summary>
+        /// <param name="target">The Target</param>
+        /// <param name="args">The Args</param>
+        private static void Orbwalker_OnPreAttack(AttackableUnit target, Orbwalker.PreAttackArgs args)
+        {
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LastHit) ||
+                Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Harass) ||
+                Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LaneClear))
+            {
+                var aaDisable = Config.FarmMenu["disableAA"].Cast<CheckBox>().CurrentValue;
+
+                if (aaDisable && !Player.Instance.HasBuff("SiphoningStrike"))
+                {
+                    args.Process = false;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Called whenever the Game Draws Itself
+        /// </summary>
+        /// <param name="args">The Args</param>
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            var drawW = Config.DrawingMenu["drawW"].Cast<CheckBox>().CurrentValue;
+            var drawE = Config.DrawingMenu["drawE"].Cast<CheckBox>().CurrentValue;
+
+            if (drawW)
+            {
+                Circle.Draw(W.IsReady() ? SharpDX.Color.Green : SharpDX.Color.Red, W.Range, Player.Instance.Position);
+            }
+
+            if (drawE)
+            {
+                Circle.Draw(E.IsReady() ? SharpDX.Color.Green : SharpDX.Color.Red, E.Range, Player.Instance.Position);
+            }
+        }
+
+        /// <summary>
+        /// Called whenever the Game Updates Itself
+        /// </summary>
+        /// <param name="args">The Args</param>
+        private static void Game_OnTick(EventArgs args)
+        {
+            if (Orbwalker.ForcedTarget != null)
+            {
+                if (!Player.Instance.IsInAutoAttackRange(Orbwalker.ForcedTarget) ||
+                    !Orbwalker.ForcedTarget.IsValidTarget() ||
+                    Orbwalker.ForcedTarget.IsInvulnerable)
+                {
+                    Orbwalker.ForcedTarget = null;
+                }
+            }
+
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo))
+            {
+                StateHandler.Combo();
+            }
+
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Harass))
+            {
+                StateHandler.Harass();
+            }
+
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LaneClear))
+            {
+                StateHandler.LaneClear();
+            }
+
+            if ((Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LastHit) ||
+                 Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Harass) ||
+                 Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LaneClear)) &&
+                Config.FarmMenu["useQ"].Cast<CheckBox>().CurrentValue)
+            {
+                StateHandler.LastHit();
+            }
+        }
+    }
+}
---
+++ KarmaPanda Addonları/Nasus/StateHandler.cs
@@ -0,0 +1,171 @@
+﻿using System;
+
+namespace Nasus
+{
+    using System.Linq;
+
+    using EloBuddy;
+    using EloBuddy.SDK;
+    using EloBuddy.SDK.Enumerations;
+    using EloBuddy.SDK.Menu.Values;
+
+    class StateHandler
+    {
+        /// <summary>
+        /// Does KillSteal
+        /// </summary>
+        public static void KillSteal(EventArgs args)
+        {
+            var useE = Config.KillStealMenu["useE"].Cast<CheckBox>().CurrentValue;
+
+            if (!useE)
+            {
+                return;
+            }
+            var target = TargetSelector.GetTarget(
+                EntityManager.Heroes.Enemies.Where(
+                    t => t.IsValidTarget(Program.E.Range) &&
+                        Extensions.DamageLibrary.CalculateDamage(t, false, true)
+                        >= t.Health), DamageType.Magical);
+
+            if (target == null) return;
+
+            Program.E.Cast(target);
+        }
+
+        /// <summary>
+        /// Does LastHit
+        /// </summary>
+        public static void LastHit()
+        {
+            if (!Program.Q.IsReady() || !Orbwalker.CanAutoAttack)
+            {
+                return;
+            }
+
+            var minion = EntityManager.MinionsAndMonsters.EnemyMinions.Where(
+                t => t.IsValidTarget(Program.Q.Range) &&
+                     Extensions.DamageLibrary.CalculateDamage(t, true, false) >= t.Health)
+                .OrderByDescending(t => t.Distance(Player.Instance))
+                .FirstOrDefault();
+
+            if (minion == null) return;
+
+            Program.Q.Cast();
+            Orbwalker.ForcedTarget = minion;
+        }
+
+        /// <summary>
+        /// Does Harass
+        /// </summary>
+        public static void Harass()
+        {
+            var useEh = Config.FarmMenu["useEH"].Cast<CheckBox>().CurrentValue;
+            var manaEh = Config.FarmMenu["manaEH"].Cast<Slider>().CurrentValue;
+
+            if (!Program.E.IsReady() || !useEh || !(Player.Instance.ManaPercent >= manaEh))
+            {
+                return;
+            }
+            var target = EntityManager.Heroes.Enemies.Where(t => Program.E.IsInRange(t) && t.IsValidTarget());
+
+            foreach (
+                var pred in
+                    target.Select(t => Program.E.GetPrediction(t))
+                        .Where(pred => pred != null)
+                        .Where(pred => pred.HitChance >= HitChance.High))
+            {
+                Program.E.Cast(pred.CastPosition);
+            }
+        }
+
+        /// <summary>
+        /// Does LaneClear
+        /// </summary>
+        public static void LaneClear()
+        {
+            var useElc = Config.FarmMenu["useELC"].Cast<CheckBox>().CurrentValue;
+            var useElcs = Config.FarmMenu["useELCS"].Cast<Slider>().CurrentValue;
+            var manaElc = Config.FarmMenu["manaELC"].Cast<Slider>().CurrentValue;
+
+            if (!Program.E.IsReady() || !useElc || !(Player.Instance.ManaPercent >= manaElc))
+            {
+                return;
+            }
+            var minion =
+                EntityManager.MinionsAndMonsters.EnemyMinions.Where(
+                    t =>
+                        Program.E.IsInRange(t) && t.IsValidTarget() &&
+                        Extensions.DamageLibrary.CalculateDamage(t, false, true) >= t.Health)
+                    .ToArray();
+
+            var pred = EntityManager.MinionsAndMonsters.GetCircularFarmLocation(minion, Program.E.Width,
+                (int) Program.E.Range);
+
+            if (pred.HitNumber >= useElcs)
+            {
+                Program.E.Cast(pred.CastPosition);
+            }
+        }
+
+        /// <summary>
+        /// Does Combo
+        /// </summary>
+        public static void Combo()
+        {
+            var useW = Config.ComboMenu["useW"].Cast<CheckBox>().CurrentValue;
+            var manaW = Config.ComboMenu["manaW"].Cast<Slider>().CurrentValue;
+
+            if (Program.W.IsReady()
+                && useW
+                && Player.Instance.ManaPercent >= manaW)
+            {
+                var target =
+                    EntityManager.Heroes.Enemies.Where(t => t.IsValidTarget(Program.W.Range))
+                        .OrderBy(t => t.Health)
+                        .FirstOrDefault();
+
+                if (target != null)
+                {
+                    Program.W.Cast(target);
+                }
+            }
+
+            var useE = Config.ComboMenu["useE"].Cast<CheckBox>().CurrentValue;
+            var manaE = Config.ComboMenu["manaE"].Cast<Slider>().CurrentValue;
+
+            if (Program.E.IsReady()
+                && useE
+                && Player.Instance.ManaPercent >= manaE)
+            {
+                var target = EntityManager.Heroes.Enemies.Where(t => Program.E.IsInRange(t)
+                                                                     && t.IsValidTarget());
+
+                foreach (
+                    var pred in
+                        target.Select(t => Program.E.GetPrediction(t))
+                            .Where(pred => pred != null)
+                            .Where(pred => pred.HitChance >= HitChance.High))
+                {
+                    Program.E.Cast(pred.CastPosition);
+                }
+            }
+
+            var useR = Config.ComboMenu["useR"].Cast<CheckBox>().CurrentValue;
+            var hpR = Config.ComboMenu["hpR"].Cast<Slider>().CurrentValue;
+            var rangeR = Config.ComboMenu["rangeR"].Cast<Slider>().CurrentValue;
+            var intR = Config.ComboMenu["intR"].Cast<Slider>().CurrentValue;
+
+            if (!Program.R.IsReady() || !useR || !(Player.Instance.HealthPercent <= hpR))
+            {
+                return;
+            }
+            var enemies = EntityManager.Heroes.Enemies.Count(t => Player.Instance.Distance(t) <= rangeR);
+
+            if (enemies >= intR)
+            {
+                Program.R.Cast();
+            }
+        }
+    }
+}
